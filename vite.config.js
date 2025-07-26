85ee258627b4bf26d40306eb8aa48b0f72749d25
MO <mohamedxehab0@gmail.com>
2025-07-26 23:28:25 +0300

    fix the base

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,21 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   base: "/First-React-Project",
-  plugins: [react({
-    jsxRuntime: "automatic",
-    include: "**/*.jsx",
-  })],
+  plugins: [
+    react({
+      jsxRuntime: "automatic",
+      include: "**/*.jsx",
+    }),
+  ],
   server: {
     headers: {
-      'Content-Type': 'application/javascript',
+      "Content-Type": "application/javascript",
     },
   },
   esbuild: {
-    loader: 'jsx',
+    loader: "jsx",
     include: /src\/.*\.jsx?$/,
     exclude: [],
   },
